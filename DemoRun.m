289b3719b100b2369fc457230ab6fdc2826fcc2e
Han Xiao <han.xiao@aalto.fi>
2019-05-07 21:58:02 +0300

    demo run in my case

--- DemoRun.m
+++ DemoRun.m
@@ -10,9 +10,14 @@
 clear;
 
 % load DemoData.mat
-load('./DATA/DEMO_DATA/DemoData.mat');
+load('./DATA/slashdot.mat');
 
+NG = 10;
+alpha = 0.9;
+beta = 50;
 % Run enumKOCG.m
 cd KOCG
 [X_enumKOCG_cell, time] = enumKOCG(A, B, NG, alpha, beta)
 cd ..
+
+save('./output/slashdot.mat', 'X_enumKOCG_cell', 'time')
